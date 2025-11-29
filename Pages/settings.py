77d00b4b4b10ef40ac30ca05224568c43bcdb5f2
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2025-11-29 06:31:51 +0200

    edit settings page

--- Pages/settings.py
+++ Pages/settings.py
@@ -1,12 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 
 class SettingsPage(tk.Frame):
     def __init__(self, parent, on_logout=None):
         super().__init__(parent)
         self.on_logout = on_logout
-
-        tk.Label(self, text="Settings", font=("Arial", 28)).pack(pady=20)
-        tk.Button(self, text="Log Out", font=("Arial", 14), width=15, command=self._logout).pack(pady=20)
+        ttk.Button(self, text="Log Out", width=15, command=self._logout).pack(pady=20)
 
     def _logout(self):
         if self.on_logout:
